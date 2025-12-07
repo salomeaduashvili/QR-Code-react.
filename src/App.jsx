b1b1e452e9437e4bbf629cf782a10acd6f0c89c0
salome aduashvili <salome.sali9909@gmail.com>
2025-12-07 19:47:09 +0400

    create qr code with react component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./components/card/card";
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/card/card.jsx
@@ -0,0 +1,16 @@
+import "./card.css";
+export default function card() {
+  return (
+    <div>
+      <div className="card">
+        <img src="./images/image-qr-code.png" alt="qrcodeimg" />
+
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
